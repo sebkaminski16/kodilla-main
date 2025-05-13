13d6ae46c1514a39f2f1922f087676448c4dde4a
Sebastian Kaminski <sebkaminski16@gmail.com>
2025-05-13 17:10:14 +0200

    Fix OddNumbersExterminator.java class

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,11 +1,11 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class OddNumbersExterminator {
     public List<Integer> exterminate(List<Integer> numbers){
-        List<Integer> list = numbers;
+        List<Integer> list = new ArrayList<>(numbers);
         list.removeIf(integer -> integer % 2 != 0);
         return list;
     }
