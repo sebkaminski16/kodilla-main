c5797ef89a9de0848b8a27aedb539d4be6a0e4fb
Sebastian Kaminski <sebkaminski16@gmail.com>
2025-05-19 18:41:47 +0200

    7.2 Immutable ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,15 +1,17 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("mojadresemail", s -> s+ "@gmail.com");
-        poemBeautifier.beautify("MALY TEKST", String::toLowerCase);
-        poemBeautifier.beautify("Dzien dobry",  s -> s+"!!!!!!!");
-        poemBeautifier.beautify("Jak sie masz",  s -> "¿"+s+"?");
-        poemBeautifier.beautify("Otoczony tekst", s -> "------" + s + "-------");
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
     }
 }
